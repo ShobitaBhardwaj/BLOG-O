82bd4b4d4770638f9abbee15522d0f16bdc020d2
ShobitaBhardwaj <135360288+ShobitaBhardwaj@users.noreply.github.com>
2024-02-04 14:31:01 +0530

    Comments added

--- backend/controller/post-controller.js
+++ backend/controller/post-controller.js
@@ -14,12 +14,67 @@ export const createPost = async (request, response) => {
 }
 
 export const getAllPosts = async(request,response) =>{
+    let category = request.query.category;
+    let posts ;
     try{
+        if(category) {
+            posts = await Post.find({ categories : category})
+        } else {
+            posts = await Post.find({});
+        }
         //This empty conditions shows that it wll fetch entire data
-        let posts = await Post.find({});
+
         return response.status(200).json({ posts });
 
     } catch (error){
         return response.status(500).json({ msg : error.message });
+    }
+}
+
+
+export const getPost = async(request,response) => {
+    try{
+        const post = await Post.findById(request.params.id);
+        return response.status(200).json(post);
+    } catch ( error ){
+        return response.status(500).json({ msg: error.message});
+    }
+}
+
+export const updatePost = async( request,response) => {
+    try {
+        const post = await Post.findById(request.params.id);
+
+        if(!post){
+            return response.status(404).json({ mag : 'Post not found'});
+        }
+
+        await Post.findByIdAndUpdate(request.params.id , {$set: request.body})
+        //$ set to replace an object inside an array
+        //$ addToSet to append an object to an array
+        return response.status(200).json({ mag : 'Post updated'});
+
+    } catch ( error ) {
+        return response.status(500).json({ error : error.message });
+
+    }
+}
+
+
+export const deletePost = async (request,response) =>{
+    try{
+        const post = await Post.findById(request.params.id);
+
+        if(!post) {
+            return response.status(404).json({ msg: 'Post not found'});
+        }
+
+        await post.delete();
+        return response.status(200).json({ msg: 'Post Deleted'});
+
+
+    } catch (error){
+        return response.status(500).json({ error : error.message  });
+
     }
 }
